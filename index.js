d90355dbcde8c8d0185e5057afafe221cfd14687
Hakxel <asiel_montes@yahoo.com>
2018-07-17 15:49:44 -0400

    Add latest changes

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function Phrase(content) {
 
   // Returns the letters in the content.
   this.letters = function letters() {
-    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+    const letterRegEx = /[a-z]/i;
+    return Array.from(this.content).filter(c => c.match(letterRegEx)).join("");
+
     // let theLetters = [];
     // const letterRegEx = /[a-z]/i;
     // Array.from(this.content).forEach(function(character) {
@@ -30,6 +32,10 @@ function Phrase(content) {
 
   // Returns true for a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.letters()) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+        return false;
+    }
   }
 }
